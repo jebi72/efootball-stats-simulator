ecd65a463df9ac0bc28834bbe404c504cc81396e
jebi72 <jebigames72@gmail.com>
2026-01-12 21:42:52 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 // sw.js
 self.addEventListener('install', (e) => {
   console.log('서비스 워커 설치 완료');
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  // 요청을 가로채지만 아무 작업도 하지 않음 (설치 요건 충족용)
+  e.respondWith(fetch(e.request));
 });
